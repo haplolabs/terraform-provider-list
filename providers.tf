429284742f8973bd44565d378202264f4e423698
Nate Thompson <nate.thompson@maxar.com>
2021-11-02 10:43:41 -0600

    Added providers.tf and .gitignore

---
+++ providers.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+//    aws-3-37-0 = {
+//      source = "hashicorp/aws"
+//      version = "3.37.0"
+//    }
+    aws = {
+      version = "3.52.0"
+    }
+    null = {
+      version = "3.1.0"
+    }
+    template = {
+      version = "2.2.0"
+    }
+    local = {
+      version = "2.1.0"
+    }
+    external = {
+      version = "2.1.0"
+    }
+    random = {
+      version = "3.1.0"
+    }
+//    postgresql-1-7-2 = {
+//      source = "hashicorp/postgresql"
+//      version = "1.7.2"
+//    }
+  }
+}
